657c90f1ba40b8039211679a6ae7ed861b7e2cb8
Tom Ribardiere <tom.ribardiere@free.fr>
2021-04-30 13:12:05 +0200

    [ADD] Création des méthodes de connexion, déconnexion et de création d'un compte

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,39 @@ import {Injectable} from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  createNewUser(email: string, password: string): Promise<void>{
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signInUser(email: string, password: string): Promise<void>{
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signOutUser(): void{
+    firebase.auth().signOut();
+  }
+
 }
