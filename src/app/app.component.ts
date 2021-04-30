e7628ee75d919cc29446be79db009104ed88cda2
Tom Ribardiere <tom.ribardiere@free.fr>
2021-04-30 13:00:13 +0200

    [ADD] Connexion avec le projet Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDle8Vxzx_-ejDnzRiynmssM6XIybVFwK0',
+      authDomain: 'bookshelves-c230c.firebaseapp.com',
+      projectId: 'bookshelves-c230c',
+      storageBucket: 'bookshelves-c230c.appspot.com',
+      messagingSenderId: '137613534316',
+      appId: '1:137613534316:web:83d06b27d31759c76ae449'
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
